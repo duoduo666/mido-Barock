c743af509b7633339026f72738f0df7917192227
duoduo666 <52728221+duoduo666@users.noreply.github.com>
2020-02-17 21:28:14 +0800

    Update play note(basic).py

--- play note/play note(basic).py
+++ play note/play note(basic).py
@@ -4,7 +4,14 @@ from mido import Message, MidiFile, MidiTrack
 #测试
 mid = MidiFile()
 track = MidiTrack()
+track2 = MidiTrack()
+track3 = MidiTrack()
+track4 = MidiTrack()
 mid.tracks.append(track)
+mid.tracks.append(track2)
+mid.tracks.append(track3)
+mid.tracks.append(track4)
+tra = [track,track2,track3,track4]
 
 def yin(yin,pai,qian=0,unit=track,tong=0,liang=64,qi=2):              #yin是指哪个音，pai是指时间（节拍），qian是noteon里的time，liang是指音量，tong是通道，
     if type(yin)== str:
@@ -18,7 +25,7 @@ def beat(time):                  #与mido的拍子互换
     time = 1/time
     return time
 
-def myin(fu,pai,time=120,chord=None,bef=None,note="geng",yue=2):   #和声版
+def myin(fu,pai,time=120,du=None,chord=None,bef=None,note="low",yue=2):   #和声版
     pig = int(beat(time))
     for i in range(len(pai)):
         if type(pai[i]) == list:
@@ -30,93 +37,156 @@ def myin(fu,pai,time=120,chord=None,bef=None,note="geng",yue=2):   #和声版
                 else:
                     yin(fu[i][j],round(pai[i][j]*pig),bef[i][j],unit=tra[j],qi=yue)
         else:
-            if chord == None:
+            if chord == None and du == None:
                 yin(fu[i],pai[i]*pig,qi=yue)
             else:
+                #和弦
                 if chord == "dasan":     #大三和弦
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = b_three(fu[i])
                     elif note == "zhong":
                         fu[i] = b_three(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = b_three(zhong=fu[i])
+                        fu[i] = b_three(wu=fu[i])
                 elif chord == "xiaosan":    #小三（没骂人）
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = s_three(fu[i])
                     elif note == "zhong":
                         fu[i] = s_three(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = s_three(zhong=fu[i])
+                        fu[i] = s_three(wu=fu[i])
                 elif chord == "zengsan":    #增三
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = z_three(fu[i])
                     elif note == "zhong":
                         fu[i] = z_three(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = z_three(zhong=fu[i])
+                        fu[i] = z_three(wu=fu[i])
                 elif chord == "jiansan":    #减三
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = j_three(fu[i])
                     elif note == "zhong":
                         fu[i] = j_three(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = j_three(zhong=fu[i])
+                        fu[i] = j_three(wu=fu[i])
                 elif chord == "dasi":      #大四
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = b_four(fu[i])
                     elif note == "zhong":
                         fu[i] = b_four(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = b_four(zhong=fu[i])
+                        fu[i] = b_four(wu=fu[i])
                 elif chord == "xiaosi":    #小四(没骂人)
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = s_four(fu[i])
                     elif note == "zhong":
                         fu[i] = s_four(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = s_four(zhong=fu[i])
+                        fu[i] = s_four(wu=fu[i])
                 elif chord == "zengsi":    #增四
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = z_four(fu[i])
                     elif note == "zhong":
                         fu[i] = z_four(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = z_four(zhong=fu[i])
+                        fu[i] = z_four(wu=fu[i])
                 elif chord == "jiansi":    #减四
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = j_four(fu[i])
                     elif note == "zhong":
                         fu[i] = j_four(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = j_four(zhong=fu[i])
+                        fu[i] = j_four(wu=fu[i])
                 elif chord == "daliu":     #大四六
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = b_six(fu[i])
                     elif note == "zhong":
                         fu[i] = b_six(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = b_six(zhong=fu[i])
+                        fu[i] = b_six(wu=fu[i])
                 elif chord == "xiaoliu":   #小四六
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = s_six(fu[i])
                     elif note == "zhong":
                         fu[i] = s_six(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = s_six(zhong=fu[i])
+                        fu[i] = s_six(wu=fu[i])
                 elif chord == "zengliu":   #增四六
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = z_six(fu[i])
                     elif note == "zhong":
                         fu[i] = z_six(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = z_six(zhong=fu[i])
+                        fu[i] = z_six(wu=fu[i])
                 elif chord == "jianliu":   #减四六
-                    if note == "geng":
+                    if note == "low":
                         fu[i] = j_six(fu[i])
                     elif note == "zhong":
                         fu[i] = j_six(zhong=fu[i])
                     elif note == "wu":
-                        fu[i] = j_six(zhong=fu[i])
+                        fu[i] = j_six(wu=fu[i])
+                #音程（度）
+                if du == "xiaoer":                   #小二度
+                    if note == "low":
+                        fu[i] = sd_two(fu[i])
+                    if note == "high":
+                        fu[i] = sd_two(high=fu[i])
+                if du == "daer":                     #大二度
+                    if note == "low":
+                        fu[i] = bd_two(fu[i])
+                    if note == "high":
+                        fu[i] = bd_two(high=fu[i])
+                if du == "xiaosan":                 #小三度              
+                    if note == "low": 
+                        fu[i] = sd_three(fu[i])
+                    if note == "high":
+                        fu[i] = sd_three(high=fu[i])
+                if du == "dasan":                    #大三度
+                    if note == "low":
+                        fu[i] = bd_three(fu[i])
+                    if note == "high":
+                        fu[i] = bd_three(high=fu[i])
+                if du == "chunsi":                   #纯四度
+                    if note == "low":
+                        fu[i] = cd_four(fu[i])
+                    if note == "high":
+                        fu[i] = cd_four(high=fu[i])
+                if du == "zengsi":                   #增四度
+                    if note == "low":
+                        fu[i] = zd_four(fu[i])
+                    if note == "high":
+                        fu[i] = zd_four(high=fu[i])
+                if du == "chunwu" or du == "wu":     #纯五度
+                    if note == "low":
+                        fu[i] = d_five(fu[i])
+                    if note == "high":
+                        fu[i] = d_five(high=fu[i])
+                if du == "xiaoliu":                   #小六度
+                    if note == "low":
+                        fu[i] = sd_six(fu[i])
+                    if note == "high":
+                        fu[i] = sd_six(high=fu[i])
+                if du == "daliu":                     #大六度
+                    if note == "low":
+                        fu[i] = bd_six(fu[i])
+                    if note == "high":
+                        fu[i] = bd_six(high=fu[i])
+                if du == "xiaoqi":                   #小七度
+                    if note == "low":
+                        fu[i] = sd_seven(fu[i])
+                    if note == "high":
+                        fu[i] = sd_seven(high=fu[i])
+                if du == "daqi":                    #大七度
+                    if note == "low":
+                        fu[i] = bd_seven(fu[i])
+                    if note == "high":
+                        fu[i] = bd_seven(high=fu[i])
+                if du == "badu" or du == "eight" or du == "ba":      #八度
+                    if note == "low":
+                        fu[i] = d_eight(fu[i])
+                    if note == "high":
+                        fu[i] = d_eight(high=fu[i])
+                #循环
                 for x in range(len(fu[i])):
                     if bef == None:
                         yin(fu[i][x],int(pai[i]*pig),unit=tra[x],qi=yue)
